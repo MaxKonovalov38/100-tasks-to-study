3539d0018b49999d708a8dc67b82a10763f01ca2
MaxKonovalov38 <max.kon.38r@gmail.com>
2025-05-25 19:40:23 +0800

    the problem is solved

---
+++ chapter-01/example-02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Автор: MaxKonovalov38
+   Задача: Реализуйте программу, которая считывает два числа и выводит их сумму.*/
+
+int main(void) {
+	int a = 1;
+	int b = 2;
+
+	printf("a + b = %d\n", a + b);
+
+	return 0;
+}
